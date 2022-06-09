b22f7d2cf3b06fcaa2433fde6f14482c715d52f3
Radish <53844660+radixsh@users.noreply.github.com>
2022-06-09 02:14:19 -0700

    Add everything... I guess I forgot to commit before

--- index.py
+++ index.py
@@ -1,73 +1,129 @@
-import requests
-from bs4 import BeautifulSoup
 import datetime
+import os
+import webbrowser
 
 class Citation:
     def __init__(self, details):
         self.details = details
-        # self.url = url
-        # self.website_title = website_title
-        # self.date_accessed = date_accessed
-        # self.date_published = date_published
-        # self.publisher = publisher
+    def printMla(self):
+        filename = 'mla.html'
+        f = open(filename, 'w')
+        html = '<html><head></head><body>'
+        html += f'{self.details["Article author"]}. '
+        html += f'"{self.details["Article title"]}." '
+        html += f'<i>{self.details["Publisher"]}</i>, '
+        html += f'{self.details["Date published"]}, '
+        html += f'{self.details["URL"]}. '
+        html += f'Accessed {self.details["Date accessed"]}. '
+        html += '</body></html>'
+        f.write(html)
+        f.close()
+        # https://stackoverflow.com/questions/40905703/how-to-open-an-html-file-in-the-browser-from-python
+        webbrowser.open('file://' + os.path.realpath(filename), new=2)
+    def printApa(self):
+        filename = 'apa.html'
+        f = open(filename, 'w')
+        html = '<html><head></head><body>'
+        html += f'{self.details["Article author"]}. '
+        html += f'({self.details["Date published"]}). '
+        html += f'<i>{self.details["Article title"]}.</i> '
+        html += f'{self.details["Publisher"]}. '
+        html += f'{self.details["URL"]}'
+        html += '</body></html>'
+        f.write(html)
+        f.close()
+        webbrowser.open('file://' + os.path.realpath(filename), new=2)
 
-# r = requests.get('https://www.newyorker.com/culture/on-and-off-the-avenue/the-allure-of-the-nap-dress-the-look-of-gussied-up-oblivion')
-r = requests.get('https://www.geeksforgeeks.org/python-web-scraping-tutorial/')
-soup = BeautifulSoup(r.content, 'html.parser')
-# https://stackoverflow.com/questions/36768068/get-meta-tag-content-property-with-beautifulsoup-and-python#36768533
-# https://www.browserling.com/tools/html-prettify
+def validate_date(date, string):
+    try:
+        datetime.datetime.strptime(date, string)
+        return date 
+    except ValueError:
+        print('Invalid date; you\'ll have to fix this by hand later.')
+        return "DATE HERE"
 
-# print('Status code: ' + str(r))
+def clean_url(url):
+    if url == '':
+        url = 'Invalid URL; you\'ll have to fix this by hand later.')
+        return url
+    url = url.replace('https://', '')
+    url = url.replace('http://', '')
+    if not url.startswith('www.'):
+        url = f'www.{url}'
+    # https://stackoverflow.com/questions/7053607/how-to-trim-characters-in-python
+    url = url.rstrip('/')
+    return url
 
-details = {} # dictionary of strings, mapping categories to values
+def mla():
+    details = {} # dictionary of strings, mapping categories to values
 
-article_title = soup.find('meta', property='og:title')
-details['Article title'] = str(article_title)
-# print('Article title: ' + details['Article title']) 
+    author = input('Article author (Last, First M): ')
+    details['Article author'] = author.title().strip()
+    print(f'{details["Article author"]}')
 
-url = soup.find('meta', property='og:url')
-details['URL'] = str(url)
+    title = input('Article title: ')
+    details['Article title'] = title.title().strip('". ')
+    print(f'{details["Article title"]}')
 
-website_title = soup.find('meta', property='og:site_name')
-details['Website title'] = str(website_title)
+    publisher = input('Publisher (e.g., The New York Times): ')
+    details['Publisher'] = publisher.title().strip('". ')
+    print(f'{details["Publisher"]}')
 
-# https://www.w3schools.com/python/python_datetime.asp
-date_accessed = datetime.datetime.now()
-date_accessed = date_accessed.strftime('%B %d, %Y')
-details['Date accessed'] = date_accessed
+    date_published = input('Date published (e.g., 22 May 2007): ')
+    mla_date_format = "%d %B %Y"
+    details['Date published'] = validate_date(date_published, mla_date_format).strip()
+    print(f'{details["Date published"]}')
 
-date_published = soup.find('meta', property='article:published_time')
-details['Date published'] = date_published
+    url = input('URL: ')
+    details['URL'] = clean_url(url)
+    print(f'{details["URL"]}')
 
-# ask user??
-publisher = input('Publisher, if you know it (this is really hard to find\
-        programmatically, sorry): ')
-details['Publisher'] = publisher
-print('Publisher: ' + details['Publisher'])
-
-# mycitation = Citation(details)
-
-print("0: Quit")
-print("1: Article title")
-print("2: Website title")
-print("3: Date accessed")
-print("4: Date published")
-print("5: Publisher")
-thing = input('Add in any information you have...')
-while thing:
-    thing = input('Add in any information you have...')
-    if thing == 1:
-        details['Article title'] = input('Article title: ')
-    elif thing == 2:
-        string = input('Website title: ')
-        details['Website title'] = string 
-    elif thing == 3:
-        details['Date accessed'] = input('Date accessed: ')
-    elif thing == 4:
-        details['Date published'] = input('Date published: ')
-    elif thing == 5:
-        details['Publisher'] = input('Publisher: ')
+    # https://www.w3schools.com/python/python_datetime.asp
+    date_accessed = input('Date accessed (if empty, default is today): ')
+    if not date_accessed:
+        date_accessed = datetime.datetime.now().strftime(mla_date_format)
+        details['Date accessed'] = date_accessed
     else:
-        break
+        details['Date accessed'] = validate_date(date_accessed, mla_date_format) 
+    print(f'{details["Date accessed"]}')
 
-print(details)
+    mycitation = Citation(details)
+    mycitation.printMla()
+
+# https://owl.purdue.edu/owl/research_and_citation/apa_style/apa_formatting_and_style_guide/reference_list_electronic_sources.html
+def apa():
+    details = {} # dictionary of strings, mapping categories to values
+
+    author = input('Article author (Last, F. M.): ')
+    details['Article author'] = author.title().rstrip(' .');
+    print(f'{details["Article author"]}')
+
+    date_published = input('Date published (e.g., 2003, November 18): ')
+    details['Date published'] = validate_date(date_published, "%Y, %B %d").strip()
+    print(f'{details["Date published"]}')
+
+    title = input('Article title: ')
+    details['Article title'] = title.strip('". ')
+    print(f'{details["Article title"]}')
+
+    site_name = input('Site name (e.g., Medium): ')
+    details['Publisher'] = site_name.title().strip('". ')
+    print(f'{details["Publisher"]}')
+
+    url = input('URL: ')
+    details['URL'] = clean_url(url)
+    print(f'{details["URL"]}')
+
+    mycitation = Citation(details)
+    mycitation.printApa();
+
+if __name__ == "__main__":
+    citation_type = input("Which citation type you want today? (mla, chicago, apa): ")
+    if citation_type == "mla":
+        mla()
+    elif citation_type == "chicago":
+        chicago()
+    elif citation_type == "apa":
+        apa()
+    else:
+        print("Unrecognized citation type :(")
